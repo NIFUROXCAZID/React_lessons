636f0f78ffb7f5fffb7d5ed958b12746777456a3
Amosov Vladislav <63251628+NIFUROXCAZID@users.noreply.github.com>
2026-02-15 18:02:33 +0200

    Delete test-app-1 directory

--- test-app-1/src/components/task-1/index.js
+++
@@ -1,4 +0,0 @@
-export { default } from './task-1'
-
-export { JustLorem2 } from './task-1'
-export { JustLorem3 } from './task-1'
--- test-app-1/src/components/task-2/task-2.jsx
+++
@@ -1,6 +0,0 @@
-export default function task2() {
-
-    return (
-        <p>task222222222222222222222 task2 task2 task2</p>
-    )
-}
